319185d58d3146af5df257527a89acf10f2072cb
GeorgePires <georgeasp.pi@gmail.com>
2023-04-19 00:32:41 -0300

    :sparkles: feat: add action text in view

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,8 @@
 class Post < ApplicationRecord
   belongs_to :user
 
+  has_rich_text :body
+
   validates :title, presence: true, length: { minimum: 5, maximum: 50 }
   validates :body, presence: true, length: { minimum: 12, maximum: 1000 }
 end
