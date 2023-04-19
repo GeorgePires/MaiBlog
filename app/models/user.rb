ffd19e89c1ba40851ed004f98a77fb7748eba5c5
GeorgePires <georgeasp.pi@gmail.com>
2023-04-19 18:31:59 -0300

    :sparkles: feat: create views components

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,8 @@ class User < ApplicationRecord
 
   validates :first_name, :last_name, presence: true
   validates :first_name, length: { minimum: 3 }
+
+  def full_name
+    "#{self.first_name} #{self.last_name}"
+  end
 end
