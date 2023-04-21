9a8ddcc624caa349fe6fc8c883c0b6d4806551fd
GeorgePires <georgeasp.pi@gmail.com>
2023-04-21 00:25:29 -0300

    :sparkles: feat: add comments controller

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+class CommentsController < ApplicationController
+  before_action :set_post
+
+  def create
+    @comment = @post.comments.create(comment_params)
+    @comment.user = current_user
+
+    respond_to do |format|
+      if @comment.save
+        format.html { redirect_to post_url(@post), notice: 'Comment was successfully created.' }
+      else
+        format.html { rendirect_to post_path(@post), status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @comment = @post.comments.find(params[:id])
+    @comment.destroy
+    redirect_to post_path(@post)
+  end
+
+  private
+
+  def set_post
+    @post = Post.find(params[:post_id])
+  end
+
+  def comment_params
+    params.require(:comment).permit(:body)
+  end
+end
