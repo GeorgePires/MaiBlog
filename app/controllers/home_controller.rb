b2f07651cff0dba9664d7eb1e0f16ee8df9e367c
GeorgePires <georgeasp.pi@gmail.com>
2023-04-21 01:04:04 -0300

    :lipstick: style: add home layout

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class HomeController < ApplicationController
+  layout 'home'
+
   def index
     @posts = Post.includes(:user, :rich_text_body).order(created_at: :desc)
   end
