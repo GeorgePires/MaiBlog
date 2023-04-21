7260c5aa3228c0146038cb89587b1a20803fb13c
GeorgePires <georgeasp.pi@gmail.com>
2023-04-21 01:19:33 -0300

    :heavy_plus_sign: build: set associations

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,7 +10,9 @@ class PostsController < ApplicationController
   end
 
   # GET /posts/1 or /posts/1.json
-  def show; end
+  def show
+    @comments = @post.comments.order(created_at: :desc)
+  end
 
   # GET /posts/new
   def new
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,13 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  has_many :posts, dependent: :destroy
+  has_many :comments, dependent: :destroy
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
-  has_many :posts
 
   validates :first_name, :last_name, presence: true
   validates :first_name, length: { minimum: 3 }
