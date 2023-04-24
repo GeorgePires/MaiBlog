678bd9f6756c03a725609492805b50fac4b6da7b
GeorgePires <georgeasp.pi@gmail.com>
2023-04-24 02:41:58 -0300

    :heavy_plus_sign: build: migration remove body from posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_21_222956) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_24_050201) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -64,7 +64,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_21_222956) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
-    t.string "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
