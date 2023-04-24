2c9f540f137ffcdedc9a55053088ac9036e18669
GeorgePires <georgeasp.pi@gmail.com>
2023-04-24 19:51:39 -0300

    :recycle: refactor: assets

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -1,3 +1,5 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   content: [
     './public/*.html',
@@ -7,6 +9,13 @@ module.exports = {
     './node_modules/flowbite/**/*.js',
     './vendor/javascript/tailwindcss-stimulus-components.js'
   ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
   plugins: [
     require('@tailwindcss/forms'),
     require('@tailwindcss/aspect-ratio'),
