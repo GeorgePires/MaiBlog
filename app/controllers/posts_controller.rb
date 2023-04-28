489230638b6e2613f31f11d67023d419af76dac6
GeorgePires <georgeasp.pi@gmail.com>
2023-04-28 19:43:01 -0300

    :sparkles: feat: set internationalization in controllers

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -30,7 +30,7 @@ class PostsController < ApplicationController
 
     respond_to do |format|
       if @post.save
-        format.html { redirect_to post_url(@post), notice: 'Post was successfully created.' }
+        format.html { redirect_to post_url(@post), notice: t('home.create.success', model: Post.model_name.human) }
         format.json { render :show, status: :created, location: @post }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -43,7 +43,7 @@ class PostsController < ApplicationController
   def update
     respond_to do |format|
       if @post.update(post_params)
-        format.html { redirect_to post_url(@post), notice: 'Post was successfully updated.' }
+        format.html { redirect_to post_url(@post), notice: t('home.update.success', model: Post.model_name.human) }
         format.json { render :show, status: :ok, location: @post }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -57,7 +57,7 @@ class PostsController < ApplicationController
     @post.destroy
 
     respond_to do |format|
-      format.html { redirect_to posts_url, notice: 'Post was successfully destroyed.' }
+      format.html { redirect_to posts_url, notice: t('home.destroy.success', model: Post.model_name.human) }
       format.json { head :no_content }
     end
   end
